8d76cd2bc61d6ad2d52569c8d5008b1bf50a4447
sajda <sajda.kbir@gmail.com>
2023-01-06 01:14:55 +0530

    chat stripe logic implement

--- client/script.js
+++ client/script.js
@@ -18,23 +18,43 @@ function loader(element) {
 }
 
 
-function typeText(element,text){
-  let index=0;
-  let interval=setInterval(()=>{
-    if(index<text.length){
-      element.innerHTML+=text.chartAt(index);
+function typeText(element, text) {
+  let index = 0;
+  let interval = setInterval(() => {
+    if (index < text.length) {
+      element.innerHTML += text.chartAt(index);
       index++;
-    }else{
+    } else {
       clearInterval(interval);
     }
-  },20)
+  }, 20)
 }
 
 
-function generateUniqueId(){
-  const timestamp=Data.now();
-  const randomNumber=Math.random();
-  const hexadecimalString=randomNumber.toString(16);
+function generateUniqueId() {
+  const timestamp = Data.now();
+  const randomNumber = Math.random();
+  const hexadecimalString = randomNumber.toString(16);
 
   return `id-${timestamp}-${hexadecimalString}`;
+}
+
+
+function chatStripe(isAi, value, uniqueId) {
+  return (
+    `
+    <div class="wrapper ${isAi && 'ai'}">
+    <div class="chat">
+    <div class="profile">
+    <img
+    src="${isAi ? bot : user}"
+    alt="${isAi ? 'bot' : 'user'}"
+    />
+    <div class="message" id=${uniqueId}>${value}</div>
+    </div>
+    
+    </div>
+
+    `
+  )
 }
