b5670c1a3e22e38fda79e69362fe702d07268eab
sajda <sajda.kbir@gmail.com>
2023-01-06 15:19:54 +0530

    configure the open API AI

--- server/server.js
+++ server/server.js
@@ -24,6 +24,32 @@ app.get('/',async(req,res)=>{
 })
 
 
+app.post('/',async(req,res)=>{
+    try {
+        const prompt=req.body.prompt;
+
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: `${prompt}`,
+            temperature: 0,
+            max_tokens: 3000,
+            top_p: 1,
+            frequency_penalty: 0,
+            presence_penalty: 0,
+           
+          });
+
+          res.status(200).send({
+            bot:response.data.choices[0].text
+          })
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({error})
+    }
+})
+
+
 
 
 app.listen(5000,()=>console.log('AI server started on http://localhost:5000'));
